e30f971c2a322c8292052bec9e537ffa9bc7557f
villain-ji <adirajput12309@gmail.com>
2025-01-26 20:16:28 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -67,7 +67,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ImperfectXD/SOHINI_MUSIC",
+    "https://github.com/ImperfectXD/Sohinixmusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
